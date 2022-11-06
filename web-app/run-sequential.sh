8b5e0e35159d43984b8e0ab4ac61b16d2c6cb774
Ashutosh Neupane <adhinneupane12345@gmail.com>
2022-11-06 16:08:53 -0600

    Create run-sequential.sh

---
+++ web-app/run-sequential.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tmp_file=$(mktemp /tmp/file.XXX)
+for scripts in /Users/admin/masters-project/web-app/tmp/*; do
+   sh $scripts &
+   PID="$!"
+   echo "$PID:$scripts" >> $tmp_file
+   PID_LIST+="$PID "
+done
+
+for process in ${PID_LIST[@]};do
+   wait $process
+   exit_status=$?
+   script_name=`egrep $process $tmp_file | awk -F ":" '{print $2}' | rev | awk -F "/" '{print $2}' | rev`
+   echo "$script_name exit status: $exit_status"
+done
+
+rm -f $tmp_file
