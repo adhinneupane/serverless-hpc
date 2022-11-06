265b5dfb1cf4b3fc0a1d771f756fcea62c461358
Ashutosh Neupane <adhinneupane12345@gmail.com>
2022-11-06 16:07:50 -0600

    Create execute-openwhisk.sh

---
+++ web-app/static/execute-openwhisk.sh
@@ -0,0 +1,17 @@
+cd ../serverless-hpc/web-apps/openwhisk/
+
+# docker build -t python3action:web-application-rendering .
+
+# docker tag python3action:web-application-rendering adhinneupane/python3action:web-application-rendering
+
+# docker push adhinneupane/python3action:web-application-rendering
+
+# wsk action delete web-app-wsk
+
+wsk action create web-app-wsk --docker adhinneupane/python3action:web-application-rendering action.py  
+
+wsk action invoke web-app-wsk --result
+
+wsk action invoke web-app-wsk --result | tee /Users/admin/masters-project/newfile.html
+
+
