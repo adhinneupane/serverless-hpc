14756ddc3280e07f0d0cc70cee000fd10eebcf86
Ashutosh Neupane <adhinneupane12345@gmail.com>
2022-11-06 17:38:10 -0600

    Update setup.sh

--- image-classification/raspberry-pi/setup.sh
+++ image-classification/raspberry-pi/setup.sh
@@ -1,3 +1,8 @@
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
