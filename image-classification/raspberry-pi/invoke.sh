f58405f04f67e01aa78a9caadfbcfeb3c4d9b84f
Ashutosh Neupane <adhinneupane12345@gmail.com>
2022-11-06 18:45:32 -0600

    Create invoke.sh

---
+++ image-classification/raspberry-pi/invoke.sh
@@ -0,0 +1,13 @@
+BASE='{"start":'
+START=1
+END=200
+RANGE=$START',"end":'$END
+TAIL='}'
+
+echo $BASE$RANGE$TAIL
+
+aws lambda invoke --function-name machine-learning-complete \
+--cli-binary-format raw-in-base64-out \
+--log-type Tail \
+--payload $BASE$RANGE$TAIL \
+response.json
